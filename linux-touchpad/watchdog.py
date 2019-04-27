c8e3f6cce79cc7e7de4f945355ef4fb370bc9a3a
Noah <noah@coronasoftware.net>
2019-04-27 15:44:39 -0700

    Improved device detection.

--- linux-touchpad/watchdog.py
+++ linux-touchpad/watchdog.py
@@ -1,49 +1,51 @@
 import sys
 from enum import Enum
-from pyudev import Context, Monitor
-from collections import defaultdict
-
+from pyudev import Context, Monitor, Device
+from contextlib import suppress
+from typing import Tuple, Set
 from .touchpad import TouchPad, SIGTOGGLE
 
 
-class DeviceClass(Enum):
+class DeviceType(Enum):
     TouchPad = 0
-    Mouse = 1
-    Other = 2
+    USB = 1
+    OTHER = 2
 
 
-def identify(device) -> DeviceClass:
+def identify(device: Device) -> DeviceType:
+
+    def look(device: Device, *items: Tuple[str, str]) -> Set['str']:
+        found = set()
+        for dev in [device] + list(device.ancestors):
+            for name, val in items:
+                prop = dev.attributes.get(name)
+                if prop and val in prop.decode().casefold():
+                    found.add(val)
+        return found
+
     props = look(
         device,
         ('removable', 'removable'),
         ('phys', 'usb'),
+        ('name', 'mouse'),
     )
 
-    if 'removable' in props:
-        return DeviceClass.Mouse
+    is_pci = device.find_parent('pci')
+    if {'removable', 'mouse'} & props or not is_pci:
+        return DeviceType.USB
 
     # USB but not removable probably means it's a controller
     if 'usb' in props:
-        return DeviceClass.Other
+        return DeviceType.OTHER
 
-    return DeviceClass.TouchPad
-
-
-def look(device, *items) -> set:
-    found = set()
-    for dev in [device] + list(device.ancestors):
-        for name, val in items:
-            prop = dev.attributes.get(name)
-            if prop and val in prop.decode():
-                found.add(name)
-    return found
+    return DeviceType.TouchPad
 
 
 class WatchDog:
     context = Context()
-    devices = set()
 
-    _touchpad = None
+    _devices = set()
+    _touchpad: TouchPad = None
 
     def __init__(self):
         self.monitor = Monitor.from_netlink(self.context)
@@ -51,35 +53,36 @@ class WatchDog:
 
         for dev in self.context.list_devices(subsystem='input', sys_name='mouse*'):
             cls = identify(dev)
-            if cls is DeviceClass.TouchPad:
-                self._touchpad = TouchPad(dev)
-            elif cls is DeviceClass.Mouse:
-                self.add(dev)
 
-        self.refresh()
+            if cls is DeviceType.TouchPad:
+                if self._touchpad is None:
+                    self._touchpad = TouchPad(dev)
+                else:
+                    raise ValueError('Found multiple TouchPads.')
+
+            elif cls is DeviceType.USB:
+                self._devices.add(dev)
+
+        self.update()
+
+    def __on_device(self, device):
+        action = getattr(self._devices, device.action)  # self._devices.add or self._devices.remove
+        with suppress(KeyError):
+            action(device)
+        self.update()
 
     def start(self):
         for device in iter(self.monitor.poll, None):
-            if 'mouse' in device.sys_name:
-                cls = identify(device)
-                if cls is DeviceClass.Mouse and hasattr(self, device.action):
-                    action = getattr(self, device.action)
-                    action(device)
+            valid: bool = all((
+                'mouse' in device.sys_name,
+                device.action in ('add', 'remove'),
+                identify(device) is DeviceType.USB
+            ))
+            if valid:
+                self.__on_device(device)
 
-    def add(self, device):
-        self.devices.add(device)
-        self.refresh()
-
-    def remove(self, device):
-        if device in self.devices:
-            self.devices.remove(device)
-        self.refresh()
-
-    def refresh(self):
-        if not self._touchpad:
-            return
-
-        if self.devices and not self._touchpad.toggled:
+    def update(self):
+        if self._devices and not self._touchpad.toggled:
             self._touchpad.disable()
         else:
             self._touchpad.enable()
@@ -87,6 +90,6 @@ class WatchDog:
     def sig_handler(self, signum, frame):
         if signum == SIGTOGGLE:
             self._touchpad.toggle()
-            self.refresh()
+            self.update()
         else:
             sys.exit()
